d7f35c0c2d84d0b3447e8b9cdd156e5d0fd0c544
Matthias Kuhn <matthias@opengis.ch>
2020-09-25 14:13:05 +0200

    License fix

--- docker-scripts/build-android.sh
+++ docker-scripts/build-android.sh
@@ -6,6 +6,21 @@ export TARGET_SUFFIX="-${TARGET}"
 export SUPERBUILD_INSTALL_DIR=${DIR}/install
 export ANDROID_PLATFORM="android-21"
 
+
+# Fix licences
+DEST=$(SUPERBUILD_INSTALL_DIR)/share/doc/copyright
+mkdir -p ${DEST}
+cp /usr/share/doc/libexpat1/copyright      ${DEST}/expat-${IMAGE_NAME}.txt
+cp /usr/share/doc/libjpeg-turbo8/copyright ${DEST}/libjpeg-turbo-${IMAGE_NAME}.txt
+cp /usr/share/doc/liblzma5/copyright       ${DEST}/liblzma-${IMAGE_NAME}.txt
+cp /usr/share/doc/libpcre3/copyright       ${DEST}/pcre3-${IMAGE_NAME}.txt
+cp /usr/share/doc/libpng16-16/copyright    ${DEST}/libpng-${IMAGE_NAME}.txt
+cp /usr/share/doc/libtiff5/copyright       ${DEST}/tiff-${IMAGE_NAME}.txt
+cp /usr/share/doc/mingw-w64-x86-64-dev/copyright  ${DEST}/mingw-w64-x86-64-${IMAGE_NAME}.txt
+cp /usr/share/doc/sqlite3/copyright        ${DEST}/sqlite3-${IMAGE_NAME}.txt
+cp /usr/share/doc/zlib1g/copyright         ${DEST}/zlib-${IMAGE_NAME}.txt
+
+# Setup build dir
 mkdir ${DIR}/build
 cd ${DIR}/build
 
